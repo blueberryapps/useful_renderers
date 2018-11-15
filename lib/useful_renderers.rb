34e0bcfdd55ecb28a162a1e867a87f0175b92b03
Martin Magnusek <magnusekm@gmail.com>
2018-11-15 15:16:14 +0100

    Add rails 5 compatibility

--- lib/useful_renderers.rb
+++ lib/useful_renderers.rb
@@ -11,7 +11,7 @@ module UsefulRenderers
         filename = options[:filename] || options[:template]
         csv.extend UsefulRenderers::CsvRenderable
         data = csv.respond_to?(:to_csv) ? csv.to_csv(options) : csv
-        send_data data, type: Mime::CSV, disposition: "attachment; filename=#{filename}.csv"
+        send_data data, type: Mime[:csv], disposition: "attachment; filename=#{filename}.csv"
       end
 
       ActionController.add_renderer :zip do |zip, options|
